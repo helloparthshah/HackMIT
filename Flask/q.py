9dad75baa8918e08cf0ca8e105e7d1034ffe9edb
helloparthshah <helloparthshah@gmail.com>
2020-09-19 22:57:42 -0700

    Fixed autocomplete error

--- Flask/q.py
+++ Flask/q.py
@@ -1,10 +1,17 @@
 import autocomplete
 
 
-def getpred(s):
-    return autocomplete.split_predict(s)
+def getacc(s):
+    print(s)
+
+    preds = autocomplete.split_predict(s)
+    print(preds)
+    if(len(preds) == 0):
+        # return s.trim().split(" ").splice(-1)
+        return s.strip().split()[-1]
+    return preds[0][0]
 
 
 autocomplete.load()
 
-print(getpred('I am a huge '))
+print(getacc('I am a huge agi'))
--- Flask/server.py
+++ Flask/server.py
@@ -27,8 +27,9 @@ def getacc(s):
     preds = autocomplete.split_predict(s)
     print(preds)
     if(len(preds) == 0):
-        #return s.trim().split(" ").splice(-1)
-        return s.strip()
+        # return s.trim().split(" ").splice(-1)
+        # return s.strip()
+        return s.strip().split()[-1]
     return preds[0][0]
 
 
